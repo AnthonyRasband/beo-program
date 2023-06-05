e8a5bb43f2d58563603758f65f95c34cb3a8cc3a
Anthony Rasband <redflamesilk@gmail.com>
2023-06-05 09:22:36 -0600

    final again

--- finalproject/script.js
+++ finalproject/script.js
@@ -1,70 +1,70 @@
 $(document).ready(function() {
-    let counter = 0;
+  let count = 0;
 
-    $('#commentForm').on('submit', function(event) {
-        event.preventDefault();
+  $('#commentForm').on('submit', function(event) {
+      event.preventDefault();
 
-        const displayName = $('#displayNameInput').val();
-        const comment = $('#commentInput').val();
+      const name = $('#nameInput').val();
+      const comment = $('#commentInput').val();
 
-        if (displayName === "" || comment === "") {
-            return alert("There are empty inputs");
-        }
-        counter++;
-        const newCard = `
-            <div id="${counter}" class="card"> 
-            <button class="editButton">Edit</button>
-            <button class="deleteButton">Delete</button>
-                <div class="name">${displayName}</div>
-                <div class="comment">${comment}</div>
-            </div>
-        `; 
-
-        if (counter === 1) {
-            $('.originalPost').html(newCard);
-            $('.originalPost').addClass('finalOriginalPost');
-            $('.editButton').css('display','none');
-            $('.deleteButton').css('display','none');
-        } else {
-            $('#mainContainer').prepend(newCard);
-        }
-        $('#displayNameInput').val('');
-        $('#commentInput').val('');
-    });// lining up cards with prepend
-
-    $(document).on('click', '.editButton', function() {
-        const card = $(this).closest('.card');
-        const commentText = card.find('.comment').text();
-        const postId = card.attr('id');
-        const editForm = `
-          <div class="editContainer">
-            <div class="name">${card.find('.name').text()}</div>
-            <div class="comment">${commentText}</div>
-            <form class="editForm">
-              <input class="editCommentInput" value="${commentText}">
-              <button type="submit" class="saveButton">Submit</button>
-            </form>
+      if (name === "" || comment === "") {
+          return alert("There are empty inputs");
+      }
+      count++;
+      const newCard = `
+          <div id="${count}" class="card"> 
+          <button class="editButton">Edit</button>
+          <button class="deleteButton">Delete</button>
+              <div class="name">${name}</div>
+              <div class="comment">${comment}</div>
           </div>
-        `;
-    
-        card.html(editForm);
-    
-        card.find('.saveButton').on('click', function(event) {
-          event.preventDefault();
-          const editedComment = card.find('.editCommentInput').val();
-          card.find('.comment').text(editedComment); 
-          card.html(`
-            <button class="editButton">Edit</button>
-            <button class="deleteButton">Delete</button>
-            <div class="name">${card.find('.name').text()}</div>
-            <div class="comment">${editedComment}</div>
-            
-          `);
-        });
+      `; 
+
+      if (count === 1) {
+          $('.originalPost').html(newCard);
+          $('.originalPost').addClass('finalOriginalPost');
+          $('.editButton').css('display','none');
+          $('.deleteButton').css('display','none');
+      } else {
+          $('#mainContainer').prepend(newCard);
+      }
+      $('#nameInput').val('');
+      $('#commentInput').val('');
+  });// lining up cards with prepend
+
+  $(document).on('click', '.editButton', function() {
+      const card = $(this).closest('.card');
+      const commentText = card.find('.comment').text();
+      const postId = card.attr('id');
+      const editForm = `
+        <div class="editContainer">
+          <div class="name">${card.find('.name').text()}</div>
+          <div class="comment">${commentText}</div>
+          <form class="editForm">
+            <input class="editCommentInput" value="${commentText}">
+            <button type="submit" class="saveButton">Submit</button>
+          </form>
+        </div>
+      `;
+  
+      card.html(editForm);
+  
+      card.find('.saveButton').on('click', function(event) {
+        event.preventDefault();
+        const Edit = card.find('.editCommentInput').val();
+        card.find('.comment').text(Edit); 
+        card.html(`
+          <button class="editButton">Edit</button>
+          <button class="deleteButton">Delete</button>
+          <div class="name">${card.find('.name').text()}</div>
+          <div class="comment">${Edit}</div>
+          
+        `);
       });
-    
-      $(document).on('click', '.deleteButton', function() {
-        const card = $(this).closest('.card');
-        card.remove();
-      });
-    });  
+    });
+  
+    $(document).on('click', '.deleteButton', function() {
+      const card = $(this).closest('.card');
+      card.remove();
+    });
+  });  
